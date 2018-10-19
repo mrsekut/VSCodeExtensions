967bc947b095de834229e1be4a10c89284c0fc53
mrsekut <k.cloudspider@gmail.com>
2018-10-20 00:04:31 +0900

    [Add]previewhtml template

---
+++ practice/previewhtml/src/extension.ts
@@ -0,0 +1,15 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "previewhtml" is now active!');
+
+  let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    vscode.window.showInformationMessage('Hello World!');
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
